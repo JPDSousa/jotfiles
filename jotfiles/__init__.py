b682ee0987b1a68b82f27f80ccbdd4d11bc40f59
João Sousa <joao.sousa@feedzai.com>
2021-03-26 15:38:51 +0000

    Introduce fask and trello integration

--- jotfiles/__init__.py
+++ jotfiles/__init__.py
@@ -1,3 +1,22 @@
 from . import __meta__
 
 __version__ = __meta__.version
+
+from dataclasses import dataclass
+
+from flask import Flask
+
+from .trello.flask import blueprint as trello_bp
+
+
+@dataclass
+class Config:
+    pass
+
+
+def create_app(config: Config) -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(config)
+    app.register_blueprint(trello_bp)
+
+    return app
